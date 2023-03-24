e2d6500347944c9d3a0ce5313b0f7849c736a61f
Gerardo Gastelum <gegage98@gmail.com>
2023-03-24 03:52:57 -0600

    Finish navbar

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+// Boton para navbar
+
+const btnMenu = document.querySelector('.btn__open')
+const btnClose = document.querySelector('.btn__close')
+const Menu = document.querySelector('.navbar__menu')
+
+btnMenu.onclick = function () {
+  Menu.classList.add('show--menu')
+}
+
+btnClose.onclick = function () {
+  Menu.classList.remove('show--menu')
+}
